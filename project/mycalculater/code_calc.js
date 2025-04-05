23e06bb3237d4d3d7e0641e68925967777230bbd
nichego3 <152015307+Draconcu1473@users.noreply.github.com>
2025-04-05 14:34:08 +0300

    rrrr

---
+++ project/mycalculater/code_calc.js
@@ -0,0 +1,16 @@
+let x = 4
+let y = 5
+let result
+
+alert("пОСЧИТАЕМ ")
+x = +prompt("Введите первое число:")
+y = +prompt("Введите второе число:")
+
+result = x+y
+result1 = x*y
+result2 = x/y
+result3 = x-y
+alert(` ${x} + ${y} = ${result}`)
+alert(` ${x} - ${y} = ${result3}`)
+alert(` ${x} * ${y} = ${result1}`)
+alert(` ${x} : ${y} = ${result2}`)
